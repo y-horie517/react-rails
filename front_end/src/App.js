36629fa2f33b4a655772d78c9dc56c83f3c23c23
y-horie517 <y.horie0517@gmail.com>
2019-06-23 22:52:47 +0900

    todoアプリサンプル

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+// import "./App.scss"
+
+import AddTodo from "./components/AddTodo"
+import TodoList from "./components/TodoList"
+
+import axios from "axios"
 
 class App extends React.Component {
-  componentDidMount() {
-   axios
-     .get(`${this.host}todos`)
-     .then(res => {
-       console.log(res.data)
-     })
-     .catch(data => {
-       console.log(data)
-     })
- }
+  constructor(props) {
+    super(props)
+    this.state = {
+      todos: [],
+    }
 
-// export default App;
+    this.host = "http://localhost:3001/"
+  }
+
+  componentDidMount() {
+    axios
+      .get(`${this.host}todos`)
+      .then(res => {
+        this.setState({ todos: res.data })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddTodo OnSubmitTitle={this.addTodo} />
+        <TodoList todos={this.state.todos} OnClickDelete={this.deleteTodo} />
+      </div>
+    )
+  }
+
+  deleteTodo = id => {
+    const { todos } = this.state
+
+    axios
+      .delete(`${this.host}todos/${id}`)
+      .then(res => {
+        this.setState({
+          todos: todos.filter(todo => {
+            return todo.id !== id
+          }),
+        })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+
+  addTodo = todo => {
+    const { todos } = this.state
+    axios
+      .post(`${this.host}todos`, todo)
+      .then(res => {
+        this.setState({
+          todos: [...todos, res.data],
+        })
+      })
+      .catch(data => {
+        console.log(data)
+      })
+  }
+}
+
+export default App
